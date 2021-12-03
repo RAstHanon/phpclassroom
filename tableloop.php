4a6d20aa65edeaa0f7a9195fadffdd111c0ea2bb
RAstHanon <89377720+RAstHanon@users.noreply.github.com>
2021-12-03 13:36:45 +0700

    Create tableloop.php

---
+++ tableloop.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <table border="0">
+      <?php
+        for($i=0;$i<100;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
